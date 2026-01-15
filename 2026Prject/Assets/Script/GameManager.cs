5828541c4d24f41c8d71184a73ce0ffb7f16b33f
Moyung <sdh240201@sdh.hs.kr>
2026-01-15 12:02:25 +0900

    싱클톤 패턴

---
+++ 2026Prject/Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using JetBrains.Annotations;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager instance;
+    
+    public int playerHp;
+    public int playerDamage;
+
+    private void Awake()
+    {
+        if (instance == null )
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        playerHp = 100;
+        playerDamage = 1;
+    }
+}
