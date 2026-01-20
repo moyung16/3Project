4d432cc8900ff717557155806c9cba5df23bed78
Moyung <sdh240201@sdh.hs.kr>
2026-01-20 21:56:30 +0900

    플레이어 이동

---
+++ 2026Prject/Assets/Script/PlayerManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+
+    public float moveSpeed;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    void PlayerMove()
+    {
+        float h = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.position += new Vector3(h, v, 0);
+    }
+}
